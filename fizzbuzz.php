db4ea8561920017e923d8cc9c638963cd7539e9b
Dave <devlshone@gmail.com>
2020-10-26 21:29:32 -0400

    Create fizzbuzz.php

---
+++ fizzbuzz.php
@@ -0,0 +1,11 @@
+for ($i = 1; $i <= 100; $i++) {
+    $out = $i . "\n";
+if (($i % 15) === 0) {
+   $out = "FizzBuzz\n";
+} elseif (($i % 5) === 0) {
+   $out = "Buzz\n";
+} elseif (($i % 3) === 0) {
+    $out = "Fizz\n";
+}
+echo $out;
+}
